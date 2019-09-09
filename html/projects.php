4f35aa224d77c8ad98f166d1a5dbae7506d7a877
frotoe95 <mjbraun@ualberta.ca>
2019-09-09 16:53:16 -0600

    Fixed some stuff

--- html/projects.php
+++ html/projects.php
@@ -4,24 +4,34 @@
 <?php include('head.php'); ?>
 <?php include('navbar.php'); ?>
 
+<style>
+  div {
+    border:2px solid #000000;
+    background: rgb(0,0,0);
+    color: rgb(58,58,58);
+    /* width: 100px; */
+    /* float: right; */
+  }
+</style>
 <div class="content">
     <h3 style="background: none; border: none; box-shadow: none; text-align: center;">Personal Projects</h3>
-    <p style="padding-bottom: 25px">
+    
+    <p style="padding-bottom: 20px">
         I have numerous personal projects I have worked on over the years, both software and hardware related.
         I have built a custom 3D printer from scratch. It is controlled with Arduino.
         <!-- <div class="half" style="display:flex; ">
             <img src="images/3DPrinter400.png"style="height:360px; float: right;">
             <img src="images/Lion400.png" style="height:360px; ">
         </div> -->
-        <div class="split left">
-        <!-- <div class="centered"> -->
-            <img src="images/3DPrinter400.png" style="height:360px;">
-        </div>
+  <div>
+    <!-- <div class="centered"> -->
+    <img src="images/3DPrinter400.png" style="height:360px; float:left;">
+  </div>
 
-        <div class="split right">
-        <!-- <div class="centered"> -->
-            <img src="images/Lion400.png" style="height:360px;">
-        </div>
+  <div>
+    <!-- <div class="centered"> -->
+    <img src="images/Lion400.png" style="height:360px; float:right">
+  </div>
     </p>
     <!-- </div> -->
         <!-- <img src="images/3DPrintedLion.jpg" width="256" height="352" class="rotate" alt="3D Printed Lion" align="right" />
