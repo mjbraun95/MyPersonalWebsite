0d96905ef1203f5472aae3ec2f18c51d4e36816b
frotoe95 <mjbraun@ualberta.ca>
2019-09-09 08:11:15 -0600

    Fixed some stuff

--- html/projects.php
+++ html/projects.php
@@ -6,15 +6,15 @@
 
 <div class="content">
     <h3 style="background: none; border: none; box-shadow: none; text-align: center;">Personal Projects</h3>
-    <p style="padding-bottom: 1500px">
+    <p style="padding-bottom: 25px">
         I have numerous personal projects I have worked on over the years, both software and hardware related.
         I have built a custom 3D printer from scratch. It is controlled with Arduino.
     <div class="row">
         <div class="column">
-            <img src="images/3DPrintedLion.jpg" class="rotate" alt="3D Printed Lion" style="width:100%">
+            <img src="images/Lion400.png" class="rotate" alt="3D Printed Lion" style="width:100%">
         </div>
         <div class="column">
-            <img src="images/3DPrinter.png" alt="3D Printer" style="width:100%">
+            <img src="images/3DPrinter400.png" alt="3D Printer" style="width:100%">
         </div>
     </div>
         <!-- <img src="images/3DPrintedLion.jpg" width="256" height="352" class="rotate" alt="3D Printed Lion" align="right" />
