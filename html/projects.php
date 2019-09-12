7d4517183d40cff2b503619c0c82ae11de7794e4
frotoe95 <mjbraun@ualberta.ca>
2019-09-11 20:08:58 -0600

    Fixed image reizing

--- html/projects.php
+++ html/projects.php
@@ -58,12 +58,12 @@ This is a 3D printer I built with a friend from scratch. It is programmed with A
 
 <div class="content">      
     <!-- <div style="padding-top: 20px; text-align: left;"> -->
-    <div style="width: 50%; height: auto; float: left;">
+    <div style="width: 70%; height: auto; float: left;">
         <video controls class="responsiveVid">
             <source src="images/TweetHeatMute.mp4" type="video/mp4" style="float:left; margin-right: 20px;">  
         </video>
     </div>
-    <div style="width: 50%; height: auto; float: right;">
+    <div style="width: 31%; height: auto; float: right;">
         <p style="padding-left: 20px; margin: 0 auto; float: right">
         This project was made for a hackathon in November of 2018. It's a program that grabs live Tweets from Twitter which have location data, using an API key. Then, it plots those locations onto a heat map across North America, refreshing every 5 seconds. The more red the area is, the more people are tweeting from that area.
         </p>
