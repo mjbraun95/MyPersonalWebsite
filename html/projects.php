f3a713b074cfe080168fc383099c0eb12ac8f436
frotoe95 <mjbraun@ualberta.ca>
2019-09-11 19:51:31 -0600

    Fixed image reizing

--- html/projects.php
+++ html/projects.php
@@ -57,13 +57,13 @@ This is a 3D printer I built with a friend from scratch. It is programmed with A
 </div>
 
 <div class="content" style="justify-items: left">      
-    <div style="padding-top: 20px; text-align: left;">
-        <video controls class="responsiveVid">
-            <source src="images/TweetHeatMute.mp4" type="video/mp4" style="float:left; margin-right: 20px;">  
-        </video>
-        <p style="padding-left: 20px; margin: 0 auto;">
-        This project was made for a hackathon in November of 2018. It's a program that grabs live Tweets from Twitter which have location data, using an API key. Then, it plots those locations onto a heat map across North America, refreshing every 5 seconds. The more red the area is, the more people are tweeting from that area.
-        </p>
+    <!-- <div style="padding-top: 20px; text-align: left;"> -->
+    <video controls class="responsiveVid">
+        <source src="images/TweetHeatMute.mp4" type="video/mp4" style="float:left; margin-right: 20px;">  
+    </video>
+    <p style="padding-left: 20px; margin: 0 auto;">
+    This project was made for a hackathon in November of 2018. It's a program that grabs live Tweets from Twitter which have location data, using an API key. Then, it plots those locations onto a heat map across North America, refreshing every 5 seconds. The more red the area is, the more people are tweeting from that area.
+    </p>
         <!-- <br>
         <p style="padding-left: 50px; margin: 0px;">
 
@@ -72,7 +72,7 @@ This is a 3D printer I built with a friend from scratch. It is programmed with A
         <p style="padding-left: 50px; margin: 0px;">
 
         </p> -->
-    </div>
+    <!-- </div> -->
 </div>
 
 
