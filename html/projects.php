166ce4b5773c3c0f70e4c1f4e3be9d87c6d8a7a2
mjbraun95 <mjbraun@ualberta.ca>
2019-09-12 14:49:50 -0600

    Added some things

--- html/projects.php
+++ html/projects.php
@@ -13,7 +13,6 @@
 //     var container = $(window);
 //     var top = -this.height() / 2;
 //     var left = -this.width() / 2;
-//
 //     return this.css('position', 'absolute').css({ 'margin-left': left + 'px', 'margin-top': top + 'px', 'left': '50%', 'top': '50%' });
 // }
 </script>
