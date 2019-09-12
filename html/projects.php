512ae581c761d2d06eca3ded17875ec5640b0122
frotoe95 <mjbraun@ualberta.ca>
2019-09-11 18:47:42 -0600

    Fixed image reizing

--- html/projects.php
+++ html/projects.php
@@ -9,12 +9,12 @@
 </head>
 
 <script type="text/javascript">
-jQuery.fn.center = function() {
-    var container = $(window);
-    var top = -this.height() / 2;
-    var left = -this.width() / 2;
-    return this.css('position', 'absolute').css({ 'margin-left': left + 'px', 'margin-top': top + 'px', 'left': '50%', 'top': '50%' });
-}
+// jQuery.fn.center = function() {
+//     var container = $(window);
+//     var top = -this.height() / 2;
+//     var left = -this.width() / 2;
+//     return this.css('position', 'absolute').css({ 'margin-left': left + 'px', 'margin-top': top + 'px', 'left': '50%', 'top': '50%' });
+// }
 </script>
 <style>
     div.left 
