97a12b94b665fe8af396a5bb6d93e61360350b39
mjbraun95 <mjbraun@ualberta.ca>
2019-09-12 19:40:40 -0600

    Added some things

--- html/projects.php
+++ html/projects.php
@@ -135,7 +135,7 @@
         <p>
             This project was made for a hackathon in November of 2018. It's a program that grabs live Tweets from Twitter which have location data, using an API key. Then, it plots those locations onto a heat map across North America, refreshing every 5 seconds. The more red the area is, the more people are tweeting from that area.
         </p>
-    </div>
+    </div>L
     <div>
         <video controls class="responsiveVid">
             <source src="images/TweetHeatMute.mp4" type="video/mp4" style="float:left; margin-right: 20px;">  
