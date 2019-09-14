a953979c762cc6451f85c9a43cdae15dafd96def
frotoe95 <mjbraun@ualberta.ca>
2019-09-13 21:09:20 -0600

    Added new resume, cleaned things up

--- html/index.php
+++ html/index.php
@@ -9,7 +9,7 @@
 <div class="content">
     
     <p>
-        Hi, I'm Matthew Braun. Welcome to my website! I am a third year Computer Engineering Co-op student, currently attending the University of Alberta. I am looking for a Co-op work term placement starting in January 2020.
+        Hello, and welcome to my website! I am a third year Computer Engineering Co-op student, currently attending the University of Alberta. I am looking for a Co-op work term placement from January to August 2020.
     </p>
         
 </div>
