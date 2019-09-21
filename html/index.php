1ffcafcd848c44af4c192f3fec1bb168f788a35b
frotoe95 <mjbraun@ualberta.ca>
2019-09-21 06:13:39 -0600

    Fixed about section

--- html/index.php
+++ html/index.php
@@ -8,11 +8,11 @@
 
 <div class="content">
     
-<p class="fullBox" style="padding-bottom: 20px; margin: 0 auto;">
+    <p class="fullBox" style="padding-bottom: 20px; margin: 0 auto;">
         Welcome to my website! I am a third year Computer Engineering Co-op student, currently attending the University of Alberta. I am looking for an 8 month work term placement starting in January 2020, preferably doing software development. Currently, I am working on developing a small android game. I've also been learning a lot about web development lately. Not only have I been putting time into this website, I'm also working on another one for a student group I'm in, Space Alberta Exploration Robotics. If you would like references from me, or have any other questions, please let me know by email or by phone!
     </p>
     <div class="fullBox">
-        <img src="images/Doggo.jpg" class="responsive">
+        <img src="images/Doggo.png" class="responsive">
     </div>
 </div>
 </body>
