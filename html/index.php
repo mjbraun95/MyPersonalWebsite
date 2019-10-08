f9de2ef49b9ebd204dc2fb7348cbe01c33e89da3
mjbraun95 <mjbraun@ualberta.ca>
2019-10-08 17:52:01 -0600

    updated home page

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
 <div class="content">
     
     <p class="fullBox" style="padding-bottom: 20px; margin: 0 auto;">
-        Welcome to my website! I am a third year Computer Engineering Co-op student, currently attending the University of Alberta. I am looking for an 8 month work term placement starting in January 2020, preferably doing software development. Currently, I am working on developing a small android game. I've also been learning a lot about web development lately. Not only have I been putting time into this website, I'm also working on another one for a student group I'm in, Space Alberta Exploration Robotics. If you would like references from me, or have any other questions, please let me know by email or by phone!
+        Welcome to my website! I'm a third year Computer Engineering Co-op student, currently attending the University of Alberta. Currently I'm available for an 8 month work placement starting in January 2020, preferably doing software development. I've been learning a lot about web development lately. Along with this website, I have a couple of student groups which I have been doing some web development work on. If you would like references from me, or have any other questions, please let me know by email or by phone!
     </p>
     <div class="fullBox">
         <img src="images/Doggo.png" class="responsive">
