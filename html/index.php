7ae098dddc1d296ae808b2c3d1122c42fe6ca13e
Carlos <mjbraun@ualberta.ca>
2020-04-18 19:28:04 -0600

    Editing front page

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
 <div class="content">
     
     <p class="fullBox" style="padding-bottom: 20px; margin: 0 auto;">
-        Welcome to my website! I'm a third year Computer Engineering Co-op student, currently attending the University of Alberta. Currently I'm available for an 8 month work placement starting in January 2020, preferably doing software development. I've been learning a lot about web development lately. Along with this website, I have a couple of student groups which I have been doing some web development work on. If you would like references from me, or have any other questions, please let me know by email or by phone!
+        Welcome to my website! I'm a third year Computer Engineering Co-op student, attending the University of Alberta. Currently I'm available for a 4 month work placement starting in May 2020. I've been learning a lot about web development lately. Along with this website, I've been working for DevFacto as a Software Consultant, doing mostly backend work, on a 4 month internship from January to April 2020. Last summer, I was working at Compugen as a Deskside Support Technician, providing IT support at several different Edmonton offices for the Government of Alberta. If you would like references from me, or have any other questions, please let me know by email or by phone!
     </p>
     <div class="fullBox">
         <img src="images/Doggo.png" class="responsive">
