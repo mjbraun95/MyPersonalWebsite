9c98955872844e5f0c913645c534f6d513f0b53b
Matthew Braun <43424316+mjbraun95@users.noreply.github.com>
2023-06-26 13:50:23 -0600

    Home page text

---
+++ html/.history/index_20230624155951.php
@@ -0,0 +1,45 @@
+<?php $title = 'About'; ?>
+<?php $currentPage = 'About'; ?>
+
+<?php include('head.php'); ?>
+<div class="nav-wrapper">
+    <?php include('navbar.php'); ?>
+</div>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+<style>
+    .responsive {
+        max-width: 100%;
+        height: auto;
+        object-fit: scale-down;
+        margin: auto;
+    }
+</style>
+
+<div class="content">
+    <p class="fullBox">
+        Hey! Welcome to my website. <br><br>
+
+        I'm a 5th year Computer Engineering Co-op student at the University of Alberta. <br>
+        I will be available for full-time employment starting July 1st, 2023. <br><br>
+
+        In the Summer and Fall of 2022, I was on an 8-month internship at AESO as an Operations Engineering Student.
+
+        <!-- Last summer, I worked as a developer for a concrete & asphalt repair business. <br>
+        I developed and hosted a website for the company as well as created SQL databases to manage their assets, clients, and contracts. I also wrote some Python programs to parse this data and found ways to save costs and increase the company's profits. <br><br> -->
+
+        In winter of 2020, I worked for DevFacto as a Software Consultant.<br>
+        I did web development work for one of their largest clients, ASEBP. I mostly did back end work on ASEBP's site using C#, .NET Framework, and SQL databases. I wrote business rules and unit tests to calculate insurance eligibility, coverage, etc. for ASEBP's clients. I also did some front end work using React Framework and Typescript. During my time there, my team and I utilized agile software development practices, such as SCRUM, daily sprints, daily stand-ups, daily meetings to consult with clients face-to-face, either in-person or over Teams. The team collaborated together using Azure DevOps and I would submit pull requests to production regularly. <br><br>
+
+        Prior to that, I worked for Compugen in the summer of 2019, as an Deskside Support Technician. <br>
+        I provided IT support for the Government of Alberta (Compugen's main client at the time). This involved network diagnostics, hard drive recovery, reimaging, diagnosing faulty hardware, drivers, etc. at several different Alberta Government offices. <br><br>
+
+        In the summer of 2020, I worked for BC Hydro. I did construction work at the Site C Hydroelectric dam in Fort St. John, BC. I would work for 12 hours a day, 7 days a week, for 2-4 weeks, then get flown back home for a week. I worked with a large team repairing widespread concrete damage within two massive underground water diversion tunnels, each tunnel 30 feet in diameter and over 1km in length. This job wasn't very relevant to my program, but I hope it highlights my great work ethic.
+    
+    </p>
+    <div class="fullBox">
+        <img src="images/Doggo.png" class="responsive">
+    </div>
+</div>
+</body>
+</html>
